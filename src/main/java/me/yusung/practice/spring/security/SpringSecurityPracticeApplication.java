33fc993b6ec674aed945c1d8d821aba4aeb76acb
Yusung <qkrdbtjd15@naver.com>
2022-02-26 18:01:50 +0900

    Main

---
+++ src/main/java/me/yusung/practice/spring/security/SpringSecurityPracticeApplication.java
@@ -0,0 +1,14 @@
+package me.yusung.practice.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringSecurity 연습 Application
+ */
+@SpringBootApplication
+public class SpringSecurityPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+    }
+}
