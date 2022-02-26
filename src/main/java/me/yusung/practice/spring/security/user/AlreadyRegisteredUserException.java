9e44289ac2beaacfd8148ac1241f1682c8ea799c
Yusung <qkrdbtjd15@naver.com>
2022-02-26 18:00:57 +0900

    Custom Exception

---
+++ src/main/java/me/yusung/practice/spring/security/user/AlreadyRegisteredUserException.java
@@ -0,0 +1,14 @@
+package me.yusung.practice.spring.security.user;
+
+
+/*
+이미 등록된 유저를 재등록하려고 할 때 발생하는 Exception
+ */
+public class AlreadyRegisteredUserException extends RuntimeException{
+    public AlreadyRegisteredUserException(String message){
+        super(message);
+    }
+    public AlreadyRegisteredUserException(){
+        super("이미 등록된 유저입니다.");
+    }
+}
