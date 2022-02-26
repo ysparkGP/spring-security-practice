9897d5e67426677b1f0c68240282a780a01453fb
Yusung <qkrdbtjd15@naver.com>
2022-02-26 18:01:37 +0900

    JPA Auditing 세팅

---
+++ src/main/java/me/yusung/practice/spring/security/config/JpaAuditorConfig.java
@@ -0,0 +1,15 @@
+package me.yusung.practice.spring.security.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/*
+JPA Auditor Enable
+ */
+
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditorConfig {
+}
