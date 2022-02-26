3d8a2f7630458175907ff7b596a43006307d73f6
Yusung <qkrdbtjd15@naver.com>
2022-02-26 18:01:59 +0900

    DTO

---
+++ src/main/java/me/yusung/practice/spring/security/note/NoteRegisterDto.java
@@ -0,0 +1,13 @@
+package me.yusung.practice.spring.security.note;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NoteRegisterDto {
+
+    private String title;
+    private String content;
+}
