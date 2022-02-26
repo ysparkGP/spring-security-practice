8781f4b4e8cc112901232b2ce148327bba9f2ead
Yusung <qkrdbtjd15@naver.com>
2022-02-26 17:58:38 +0900

    Repository 세팅

---
+++ src/main/java/me/yusung/practice/spring/security/notice/NoticeRepository.java
@@ -0,0 +1,6 @@
+package me.yusung.practice.spring.security.notice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
