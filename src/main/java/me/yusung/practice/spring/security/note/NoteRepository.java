81c8fd2c5896b3ff3a709173b5acfd6bc83f79de
Yusung <qkrdbtjd15@naver.com>
2022-02-26 17:59:07 +0900

    Repository 세팅

---
+++ src/main/java/me/yusung/practice/spring/security/note/NoteRepository.java
@@ -0,0 +1,13 @@
+package me.yusung.practice.spring.security.note;
+
+import me.yusung.practice.spring.security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    List<Note> findByUserOrderByIdDesc(User user);
+
+    Note findByIdAndUser(Long id, User user);
+}
